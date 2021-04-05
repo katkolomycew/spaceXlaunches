a7d502b71f3ed241a5b8a4e88c0618d2fba9127d
Kat Kolomycew <kat.kolomycew@nology.io>
2021-04-05 10:31:46 +0100

    Components - creating and styling initial LaunchBox.

---
+++ src/components/LaunchBox/LaunchBox.js
@@ -0,0 +1,23 @@
+import Card from "react-bootstrap/Card";
+import "./LaunchBox.scss";
+
+const launchBox = (props) => {
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <div className="launch-box_wrapper-left">
+            <Card.Text className="launch-box_number">{props.number}</Card.Text>
+            <Card.Text className="launch-box_title">{props.title}</Card.Text>
+          </div>
+          <div className="launch-box_wrapper-right">
+            <Card.Text className="launch-box_date">{props.date}</Card.Text>
+            <Card.Text className="launch-box_launch">{props.launch}</Card.Text>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default launchBox;
