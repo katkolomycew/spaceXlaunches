e7c8ff4069034e1edb32fd33942a680d55b62de6
Kat Kolomycew <kat.kolomycew@nology.io>
2021-04-06 15:09:51 +0100

    Containers - initial data reload functionality.

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -17,9 +17,14 @@ import "./Layout.scss";
 class Layout extends Component {
   state = {
     launchData: [],
+    sortedLaunchDate: false,
   };
 
   componentDidMount() {
+    this.fetchLaunchData();
+  }
+
+  fetchLaunchData = () => {
     axios
       .get("https://api.spacexdata.com/v3/launches")
       .then((response) => {
@@ -27,26 +32,39 @@ class Layout extends Component {
         this.setState({ launchData: data });
       })
       .catch((error) => console.log(error));
-  }
+  };
+
+  sortDataHandler = () => {
+    console.log(this.state.launchData);
+  };
 
   render() {
     return (
       <Aux>
         <div className="top">
           <TextLogo img={TextLogoSrc} />
-          <RoundButton title="Reload Data" icon={RoundButtonIcon} />
+          <RoundButton
+            title="Reload Data"
+            icon={RoundButtonIcon}
+            clicked={this.fetchLaunchData}
+          />
         </div>
         <div className="buttons">
           <SquareButton title="Filter by Year" icon={SquareButtonFilterIcon} />
-          <SquareButton title="Sort Descending" icon={SquareButtonSortIcon} />
+          <SquareButton
+            title="Sort Descending"
+            icon={SquareButtonSortIcon}
+            clicked={this.sortDataHandler}
+          />
         </div>
         <div className="left">
           <ImgLogo img={ImgLogoSrc} />
         </div>
         <div className="right">
-          {this.state.launchData.map((d) => {
+          {this.state.launchData.map((d, i) => {
             return (
               <LaunchBox
+                key={i}
                 number={d.flight_number}
                 title={d.mission_name}
                 date={new Date(d.launch_date_utc).toLocaleDateString()}
